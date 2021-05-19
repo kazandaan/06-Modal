a810ae620acf7f8de254e6e5ce342041fd406102
xerf <lianjaijie@gmail.com>
2021-05-19 18:11:44 +0800

    first commit

---
+++ starter/script.js
@@ -0,0 +1,28 @@
+'use strict';
+
+const modal = document.querySelector('.modal');
+const closemodal = document.querySelector('.close-modal');
+const overlay = document.querySelector('.overlay');
+const showmodal = document.querySelectorAll('.show-modal');
+
+const closeModal = function () {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+};
+
+const openModal = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+
+for (let i = 0; i < showmodal.length; i++) {
+  showmodal[i].addEventListener('click', openModal);
+}
+
+closemodal.addEventListener('click', closeModal);
+
+overlay.addEventListener('click', closeModal);
+
+document.addEventListener('keydown', function (e) {
+  if (e.key === 'Escape' && !modal.classList.contains('hidden')) closeModal();
+});
